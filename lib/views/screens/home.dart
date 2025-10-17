58f88a425238558ff46ff68c3d5d1401f41f8df4
SabaFadhl <sabafadl98@gmail.com>
2025-10-17 18:40:05 +0300

    feat: Logout button

--- lib/views/screens/home.dart
+++ lib/views/screens/home.dart
@@ -28,28 +28,32 @@ class HomeScreen extends StatelessWidget{
             ListTile(
               leading:  Icon(Icons.home, color: Colors.teal),
               title:  Text('Home'),
-              
+
             ),
 
             ListTile(
               leading:  Icon(Icons.shopping_cart, color: Colors.teal),
               title:  Text('My Cart'),
-              
+
             ),
 
             ListTile(
               leading:  Icon(Icons.favorite, color: Colors.teal),
               title:  Text('Favorites'),
-              
+
             ),
 
             ListTile(
               leading:  Icon(Icons.person, color: Colors.teal),
               title:  Text('Profile'),
-              
+
             ),
+            Divider(height: 400, endIndent: 40, color: Colors.white,),
+            ListTile(
 
-
+              leading: const Icon(Icons.logout, color: Colors.red),
+              title: const Text('Logout', style: TextStyle(color: Colors.red)),
+            ),
 
           ],
         ),
