551fc710d73555f0a373881fb33df7fb6a6fcd1e
SabaFadhl <sabafadl98@gmail.com>
2025-10-21 23:59:31 +0300

    fix: overflow error in login page

--- lib/core/views/screens/login.dart
+++ lib/core/views/screens/login.dart
@@ -14,78 +14,80 @@ class _LoginScreenState extends State<LoginScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Login")),
-      body: Column(
-        children: [
-          SizedBox(height: 50),
-          Center(
-            child: Text(
-              "Welcome Back 👋",
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-          ),
-          Center(child: Text("Login to your account")),
-          SizedBox(height: 50),
-          AppTextformfield(label: 'Email'),
-          SizedBox(height: 10),
-          AppTextformfield(
-            label: 'Password',
-            isPassword: true,
-            icon: Icon(Icons.visibility_off),
-          ),
-          SizedBox(height: 10),
-          Align(
-            alignment: Alignment.centerRight,
-            child: TextButton(
-              onPressed: () {},
-              child: const Text(
-                "Forgot Password?",
-                style: TextStyle(color: Colors.teal),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            Center(
+              child: Text(
+                "Welcome Back 👋",
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
               ),
             ),
-          ),
-          const SizedBox(height: 10),
-
-          SizedBox(
-            width: 150,
-            height: 50,
-            child: ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.teal,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10),
+            Center(child: Text("Login to your account")),
+            SizedBox(height: 50),
+            AppTextformfield(label: 'Email'),
+            SizedBox(height: 10),
+            AppTextformfield(
+              label: 'Password',
+              isPassword: true,
+              icon: Icon(Icons.visibility_off),
+            ),
+            SizedBox(height: 10),
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                onPressed: () {},
+                child: const Text(
+                  "Forgot Password?",
+                  style: TextStyle(color: Colors.teal),
                 ),
               ),
-              child: const Text(
-                "Login",
-                style: TextStyle(fontSize: 16, color: Colors.white),
-              ),
             ),
-          ),
-          const SizedBox(height: 20),
+            const SizedBox(height: 10),
 
-          Center(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("Don't have an account? "),
-                InkWell(
-                  onTap: () {
-                    Navigator.pushNamed(context, "/register");
-                  },
-                  child: const Text(
-                    "Register",
-                    style: TextStyle(
-                      color: Colors.teal,
-                      fontWeight: FontWeight.bold,
-                    ),
+            SizedBox(
+              width: 150,
+              height: 50,
+              child: ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.teal,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
                 ),
-              ],
+                child: const Text(
+                  "Login",
+                  style: TextStyle(fontSize: 16, color: Colors.white),
+                ),
+              ),
             ),
-          ),
+            const SizedBox(height: 20),
 
-        ],
+            Center(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't have an account? "),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, "/register");
+                    },
+                    child: const Text(
+                      "Register",
+                      style: TextStyle(
+                        color: Colors.teal,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+          ],
+        ),
       ),
     );
   }
