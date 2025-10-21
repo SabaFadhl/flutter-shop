fc5e5eefebf970865e88c8b72066e86ebde5394f
SabaFadhl <sabafadl98@gmail.com>
2025-10-21 23:59:04 +0300

    feat: add horizental var to app text form field component

--- lib/core/views/components/app_textformfield.dart
+++ lib/core/views/components/app_textformfield.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 class AppTextformfield extends StatelessWidget {
-  AppTextformfield({super.key, required this.label, this.filled=true, this.isPassword=false, this.icon});
+  AppTextformfield({super.key, required this.label, this.filled=true, this.isPassword=false, this.icon, this.horizontal=15});
   String label;
   bool filled;
   bool isPassword;
   Widget? icon;
+  double horizontal;
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: EdgeInsets.symmetric(horizontal: 30,vertical: 15),
+      margin: EdgeInsets.symmetric(horizontal: horizontal,vertical: 10),
       child: TextFormField(
         obscureText: isPassword,
         decoration: InputDecoration(
@@ -18,7 +19,7 @@ class AppTextformfield extends StatelessWidget {
           filled: true,
           fillColor: filled ?Colors.grey[100]:null,
           border: OutlineInputBorder(
-
+            // borderSide: BorderSide(color: Colors.teal, style: BorderStyle.solid, width: 55),
             borderRadius: BorderRadius.circular(10),
           ),
         ),
