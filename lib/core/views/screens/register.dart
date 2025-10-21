c4fc509ae52f0acd6a065a3bb46393f58432f17b
SabaFadhl <sabafadl98@gmail.com>
2025-10-21 11:06:57 +0300

    feat: image icon

--- lib/core/views/screens/register.dart
+++ lib/core/views/screens/register.dart
@@ -8,12 +8,34 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterState extends State<RegisterScreen> {
+  var _image;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Register"),
         leading: Icon(Icons.arrow_back),
+      ),
+      body: SingleChildScrollView(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: CircleAvatar(
+                radius: 50,
+                backgroundColor: Colors.grey[300],
+                backgroundImage: _image != null ? FileImage(_image!) : null,
+                child: _image == null
+                    ? const Icon(Icons.camera_alt, size: 40, color: Colors.grey)
+                    : null,
+              ),
+            ),
+            const SizedBox(height: 20),
+
+          ],
+        ),
       ),
     );
   }
