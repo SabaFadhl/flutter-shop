9b1b145e92ab7991884ba8ff992e7421aef46c1a
SabaFadhl <sabafadl98@gmail.com>
2025-10-21 10:20:31 +0300

    feat: route to home page

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,7 @@ class MyApp extends StatelessWidget {
       ),
       home: LoginScreen(),
       debugShowCheckedModeBanner: false,
+      routes: {"/register": (ctx)=>HomeScreen()},
     );
   }
 }
